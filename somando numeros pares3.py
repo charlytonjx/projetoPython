d5b27073e9780c4bef80a529471c73586100445f
charlytonjx <133175964+charlytonjx@users.noreply.github.com>
2023-05-23 21:35:43 -0300

    Add files via upload

---
+++ somando numeros pares3.py
@@ -0,0 +1,14 @@
+def ehpar(n):
+    r = (n%2==0)
+    return r
+
+def somar_par(lst):
+  soma = 0
+  for num in lst:
+    if(ehpar(num)):
+      soma = soma + num
+  return soma
+        
+lista = [10, 2, 5, 7, 6, 3]
+soma = somar_par(lista)
+print(f"O somatório dos elementos pares da lista é: {soma}")
